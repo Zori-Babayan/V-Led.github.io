a54e6589ecf0e07cfbb606d3b6015d06a0824c33
Zori-Babayan <list_babayan@mail.ru>
2025-03-08 21:19:59 +0300

    mobile-menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 import ThemeSwitcher from './ThemeSwitcher.js';
+import OverlayMenu from './OverlayMenu.js';
 import defineScrollBarWidthCSSVar from './defineScrollBarWidthCSSVar.js'
 import './NavSwiper.js';
 import './BorderedGridScroll.js'
 
 new ThemeSwitcher()
+new OverlayMenu()
 
 defineScrollBarWidthCSSVar()
